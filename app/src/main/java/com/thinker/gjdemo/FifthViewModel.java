b15b2390e0d81c0cd472d3f3597c1dbd63f65095
yang <960357842@qq.com>
2020-06-19 18:09:21 +0800

    2020/6/19

---
+++ app/src/main/java/com/thinker/gjdemo/FifthViewModel.java
@@ -0,0 +1,7 @@
+package com.thinker.gjdemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class FifthViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/thinker/gjdemo/FourthViewModel.java
@@ -0,0 +1,7 @@
+package com.thinker.gjdemo;
+
+import androidx.lifecycle.ViewModel;
+
+public class FourthViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
